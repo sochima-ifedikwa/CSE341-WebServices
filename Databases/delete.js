a75a05c83177214be4d2845f6bc2ddc2aa7b392f
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-03 20:44:48 -0500

    Add EJS dependency and implement delete functionality for listings

---
+++ Databases/delete.js
@@ -0,0 +1,68 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    const uri = "mongodb+srv://ifedikwasochima:IFEdikwa2018@cluster0.zh7wawb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+
+        // DELETE ONE
+        // Check if a listing named "Cozy Cottage" exists. Run update.js if you do not have this listing.
+        await printIfListingExists(client, "Cozy Cottage");
+        // Delete the "Cozy Cottage" listing
+        await deleteListingByName(client, "Cozy Cottage");
+        // Check that the listing named "Cozy Cottage" no longer exists
+        await printIfListingExists(client, "Cozy Cottage");
+
+        // DELETE MANY
+        // Check if the listing named "Ribeira Charming Duplex" (last scraped February 16, 2019) exists
+        await printIfListingExists(client, "Ribeira Charming Duplex");
+        // Check if the listing named "Horto flat with small garden" (last scraped February 11, 2019) exists
+        await printIfListingExists(client, "Horto flat with small garden");
+        // Delete the listings that were scraped before February 15, 2019
+        await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
+        // Check that the listing named "Ribeira Charming Duplex" still exists
+        await printIfListingExists(client, "Ribeira Charming Duplex");
+        // Check that the listing named "Horto flat with small garden" no longer exists
+        await printIfListingExists(client, "Horto flat with small garden");
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function deleteListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").deleteOne({ name: nameOfListing });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}
+
+
+async function deleteListingsScrapedBeforeDate(client, date) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").deleteMany({ "last_scraped": { $lt: date } });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}
+
+
+async function printIfListingExists(client, nameOfListing) {
+    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
+
+    if (result) {
+        if (result.last_scraped) {
+            console.log(`Found a listing in the collection with the name '${nameOfListing}'. Listing was last scraped ${result.last_scraped}.`);
+        } else {
+            console.log(`Found a listing in the collection with the name '${nameOfListing}'`);
+        }
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
